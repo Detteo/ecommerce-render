a75af665658ab62d1417fd034394fd83b7b51c5c
ROSS <sebasguzman2016dg@gmail.com>
2026-06-04 00:23:40 -0500

    Actualización del ecommerce

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$dbname = 'bookstore';
-$user = 'root';
-$pass = '';
+$host   = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user   = getenv('DB_USER');
+$pass   = getenv('DB_PASS');
+$port   = (int)(getenv('DB_PORT') ?: 3306);
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 mysqli_set_charset($conn, 'utf8mb4');
 
 if (!$conn) {
